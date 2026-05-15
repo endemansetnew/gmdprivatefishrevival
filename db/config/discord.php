f0361dc5b446fcab0eec8bc35c243e8076d1b5e9
ItsMeEndemanset <bruhmoments133417@gmail.com>
2026-05-15 22:26:10 +0700

    Update discord.php

--- db/config/discord.php
+++ db/config/discord.php
@@ -1,7 +1,7 @@
 <?php
 # Insert your discord webhooks here
 # (REQUIRED)
-    $rateWebhook = '';
-    $toolWebhook = '';
-    $weeklyWebhook = '';
+    $rateWebhook = 'https://discord.com/api/webhooks/1503335781002121260/4_e_XQklnMX9bXvuR-6ECAt8BOUYsdpwgOl-rSvtrq3mAR96RYXEvr_FbH3y4NCzmiVa';
+    $toolWebhook = 'https://discord.com/api/webhooks/1503335781002121260/4_e_XQklnMX9bXvuR-6ECAt8BOUYsdpwgOl-rSvtrq3mAR96RYXEvr_FbH3y4NCzmiVa';
+    $weeklyWebhook = 'https://discord.com/api/webhooks/1503335781002121260/4_e_XQklnMX9bXvuR-6ECAt8BOUYsdpwgOl-rSvtrq3mAR96RYXEvr_FbH3y4NCzmiVa';
 ?>
